ef1cb3a882939298c5fc64a54df73396164f5345
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-03-25 18:55:29 -0400

    Updated seeds

--- seeds.js
+++ seeds.js
@@ -9,7 +9,7 @@ var data = [
     location : "Salem, New Hampshire",
     author :
     {
-        id : "5ab81b9f7bb2a7223cf44312",
+        id : "5ab828a6d2afad001413681d",
         username : "sarthaksachdeva"
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
@@ -26,7 +26,7 @@ var data = [
     location : "Medford, Massachussetts",
     author :
     {
-        id : "5ab81b9f7bb2a7223cf44312",
+        id : "5ab828a6d2afad001413681d",
         username : "sarthaksachdeva"
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
